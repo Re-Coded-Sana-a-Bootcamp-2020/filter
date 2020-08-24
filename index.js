22c17a4341e5762c9f15e4d03d386ed8d1f30e54
Muhanned Noman <Muhanned.Noman@gmail.com>
2020-08-24 13:12:50 +0300

    Create index.js

---
+++ index.js
@@ -0,0 +1,15 @@
+// You have an array with negative and positive number, filter them and push postive number to the positives array.
+
+const numbers = [-1, -4, 3, 8];
+
+let positives = [];
+
+// A smaple solution might be like this.
+// numbers.forEach((n) => {
+//   if (n > 0) {
+//     positives.push(n);
+//   }
+// });
+
+
+// Creat the above result using .filter method
